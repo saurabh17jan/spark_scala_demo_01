2790082c8d81ebb343394bf75e806938205d7607
saurabh17jan <45552150+saurabh17jan@users.noreply.github.com>
2020-06-30 15:13:00 +0530

    Update df_transformation.scala

--- source_code/word_count/src/main/scala/df_transformation.scala
+++ source_code/word_count/src/main/scala/df_transformation.scala
@@ -5,6 +5,9 @@ import org.apache.spark.sql.SparkSession
 object df_transformation {
 
   def startProcessing(spark : SparkSession, params: args_check.CommandLineArgs): Unit ={
+    /**
+     * create csv Schema
+     */
     val customSchema = StructType(Array(
       StructField("EMPNO"     , IntegerType , true),
       StructField("ENAME"     , StringType  , true),
@@ -17,6 +20,9 @@ object df_transformation {
     )
     )
 
+    /**
+     * read csv file and apply schema to it
+     */
     val df = spark.read
       .format("csv")
       .option("header", "true")
@@ -26,9 +32,16 @@ object df_transformation {
       .schema(customSchema)
       .load(params.inCSVpath)
 
+    /**
+     * see some sample data from file
+     * and its schema
+     */
     df.show(100, false)
     df.printSchema()
 
+    /**
+     * perform simple transformation
+     */
     df.groupBy(col("job"),col("HIREDATE"))
       .sum("sal")
       .orderBy(asc("job"), desc("sum(sal)"))
